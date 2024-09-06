c739e93a29eda8238b3cf9f2e67dd670106fd4a0
victor <vg54555@gmail.com>
2024-09-06 00:18:03 -0300

    eae

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(DateTime.Now.ToLongDateString());
-Console.WriteLine("Versão da aplicação: 1.0");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(DateTime.Now.ToLongDateString());
+        Console.WriteLine("Versão da aplicação: 1.0");
+    }
+}
